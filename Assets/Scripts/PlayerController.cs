f2f0e384b84ba525a404bdb45b18af385d1c4815
shaylacarter <shaylac@vt.edu>
2023-02-23 23:30:34 -0500

    Added wall jumps and wall sliding

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,9 +12,13 @@ public class PlayerController : MonoBehaviour
 
     //Used for ground checks when jumping.
     private float heightTestPlayer;
+    private float widthTestPlayer;
     private Collider2D playerCollider;
     private int layerMaskGround;
     private int jumpCount = 0;
+
+    //Used for wall sliding.
+    public bool wallSliding = false;
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +27,7 @@ public class PlayerController : MonoBehaviour
 
         playerCollider = GetComponent<Collider2D>();
         heightTestPlayer = playerCollider.bounds.extents.y + 0.05f;
+        widthTestPlayer = playerCollider.bounds.extents.x + 0.75f;
         layerMaskGround = LayerMask.GetMask("Ground");
     }
 
@@ -40,25 +45,47 @@ public class PlayerController : MonoBehaviour
             rigidBody.velocity = new Vector2(0, rigidBody.velocity.y);
         }
 
-        if(Input.GetButtonDown("Jump") && (IsGrounded() || jumpCount <= 1)) {
+        wallSliding = IsWalled();
+        if(Input.GetButtonDown("Jump") && (IsGrounded() || wallSliding || jumpCount <= 1)) {
             rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpSpeed);
+        } else if (wallSliding) {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, Mathf.Clamp(rigidBody.velocity.y, -1.5f, float.MaxValue));
         }
+
+        // if(Input.GetButtonDown("Jump") && (IsGrounded() || jumpCount <= 1)) {
+        //     rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpSpeed);
+        // }
+
     }
 
     private bool IsGrounded()
     {
-        RaycastHit2D hit = Physics2D.Raycast(playerCollider.bounds.center, Vector2.down, heightTestPlayer, layerMaskGround);
-        bool isGrounded = hit.collider != null;
-        Debug.DrawRay(playerCollider.bounds.center, Vector2.down * heightTestPlayer, isGrounded ? Color.green : Color.red, 0.5f);
+        RaycastHit2D groundHit = Physics2D.Raycast(playerCollider.bounds.center, Vector2.down, heightTestPlayer, layerMaskGround);
+        bool isGrounded = groundHit.collider != null;
+        //Debug.DrawRay(playerCollider.bounds.center, Vector2.down * heightTestPlayer, isGrounded ? Color.green : Color.red, 0.5f);
 
+        //This allows for double-jumping.
         if (!isGrounded) {
             jumpCount++;
         } else {
             jumpCount = 0;
         }
-
-        Debug.Log($"Jump count: {jumpCount}, isGrounded: {isGrounded}");
-
+        //Debug.Log($"Jump count: {jumpCount}, groundHit: {groundHit.collider != null}");
         return isGrounded;
     }
+
+        private bool IsWalled()
+    {
+        RaycastHit2D wallHitLeft = Physics2D.Raycast(playerCollider.bounds.center, Vector2.left, widthTestPlayer, layerMaskGround);
+        RaycastHit2D wallHitRight = Physics2D.Raycast(playerCollider.bounds.center, Vector2.right, widthTestPlayer, layerMaskGround);
+        bool isWalled = wallHitLeft.collider != null || wallHitRight.collider != null;
+
+        //We don't incremement jump count here because isGrounded already increments.
+        if (isWalled) {
+            jumpCount = 0;
+        }
+        //Debug.Log($"Jump count: {jumpCount}, wallHitLeft: {wallHitLeft.collider != null}, wallHitRight: {wallHitRight.collider != null}");
+        return isWalled;
+    }
+    
 }

c8b2ae0b1bb6ed2a5a6eef0617a4627071c0bfff
shaylacarter <shaylac@vt.edu>
2023-03-08 03:21:16 -0500

    Expanded demo scene

---
+++ Assets/Scripts/DoorControl.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorControl : MonoBehaviour
+{
+
+    private bool fadeOut;
+    public float fadeSpeed;
+    public GameObject doorCollider;
+    public GameObject particles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fadeOut) {
+            Color objectColor = this.GetComponent<Renderer>().material.color;
+            float fadeAmount = objectColor.a - (fadeSpeed * Time.deltaTime);
+
+            objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+            this.GetComponent<Renderer>().material.color = objectColor;
+
+            if (objectColor.a <= 0) {
+                fadeOut = false;
+            }
+        }
+        
+    }
+
+    public void Remove() {
+        fadeOut = true;
+        doorCollider.SetActive(false);
+        particles.SetActive(false);
+    }
+}
